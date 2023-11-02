2832b3d116a52cd0dbab548ea7cdae6e52616ef9
Dennis Paler <akosidencio@Denniss-MacBook-Pro.local>
2023-11-02 18:08:14 +0800

    fixed window undefiend in next hoc

--- src/withAuthentication.tsx
+++ src/withAuthentication.tsx
@@ -5,7 +5,7 @@ const withAuthentication = <P extends Record<string, unknown>>(
     Component: React.ComponentType<P>
   ): React.FC<P> => {
     const Auth: React.FC<P> = (props) => {
-      if(!isAuthenticated()) {
+      if(!isAuthenticated() && typeof window !== 'undefined') {
         window.location.pathname = '/login';
       }
       return <Component {...props} />;
